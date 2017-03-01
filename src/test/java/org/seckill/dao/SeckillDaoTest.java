59e27d20cb935b032b0099fd2119319cf27f90e5
kknd9819 <kknd9819@qq.com>
2017-03-01 16:27:39 +0800

    2

---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,42 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * 配置spring和Junit整合
+ * Created by X-man on 2017/3/1.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void queryById() throws Exception {
+         Long id = 1000L;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void queryAll() throws Exception {
+
+    }
+
+    @Test
+    public void reduceNumber() throws Exception {
+
+    }
+
+}
